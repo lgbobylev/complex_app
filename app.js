26f264bad2c85e442179fec21c75c54e42b8928a
Leonid <lgbobylev@yandex.ru>
2020-03-31 23:47:46 +0300

    router.js was created, it is workin with requests

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
+const router = require('./router');
+
 app.use(express.static('public'));
 // configure express app to use ejs html template 
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-    res.render('home-guest');
-});
+app.use('/', router);
 
 app.listen(3000, function(){
     console.log("Server is listening on port 3000");
